cba497802c642a6d663b49f2257f4fa15ed661d5
Imran <shaikemran555@gmail.com>
2026-09-08 15:38:34 -0500

    Working OIDC deploy pipeline

--- terraform/oidc.tf
+++ terraform/oidc.tf
@@ -1,13 +1,11 @@
-variable "github_repo" {
-  type        = string
-  description = "GitHub repo in owner/name form"
-  default     = "shaikemran555-ui/devops-pipeline"
+data "tls_certificate" "github" {
+  url = "https://token.actions.githubusercontent.com/.well-known/openid-configuration"
 }
 
 resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+  thumbprint_list = [data.tls_certificate.github.certificates[0].sha1_fingerprint]
 }
 
 data "aws_iam_policy_document" "github_assume" {
@@ -29,7 +27,7 @@ data "aws_iam_policy_document" "github_assume" {
     condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
-      values   = ["repo:${var.github_repo}:ref:refs/heads/main"]
+      values   = ["repo:shaikemran555-ui@*/devops-pipeline@*:ref:refs/heads/main"]
     }
   }
 }
